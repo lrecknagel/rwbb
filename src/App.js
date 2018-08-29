eece877a31337dd0592b0aa279b272bfede81c85
Lucas Recknagel <lucas91@me.com>
2018-08-29 16:05:43 +0200

    test scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import './App.scss';
 class App extends Component {
   render() {
 
     const APP_NAME = process.env.REACT_APP_NAME
 
     return (
-      <h1>HELLO at { APP_NAME }</h1>
+      <h1 className='app'>HELLO at { APP_NAME }</h1>
     );
   }
 }
